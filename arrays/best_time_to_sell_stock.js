1b79f6e02e765d1cdab3d2a8a69e48302ea98559
Simon Gaviria <simongaviria1@gmail.com>
2024-06-11 14:03:12 -0400

    add best time to sell stock DSA question to array directory

---
+++ arrays/best_time_to_sell_stock.js
@@ -0,0 +1,19 @@
+/**
+ * You are given an array prices where prices[i] is the price of a given stock on the ith day.
+ * You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+ * Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+ 
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let maxProfit = 0, min = Number.MAX_VALUE;
+    for(let i = 0; i < prices.length; i++){ 
+        if(min > prices[i]){ 
+            min = prices[i]; 
+        }else if(maxProfit < prices[i] - min){ 
+            maxProfit = prices[i] - min; 
+        }
+    }
+    return maxProfit; 
+};
